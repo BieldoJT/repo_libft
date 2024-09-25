b7e66bb0b3d3a8033d8ece03973674c692cbc54b
BieldoJT <gabrieldaconceicaodeoliveira@gmail.com>
2024-09-25 00:52:30 -0300

    consegui fazer a memmove AAAAAAAAAAAAAAAAA

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,44 +2,35 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*char_dest;
-	const unsigned char	*char_src;
-	size_t	i;
+	unsigned char *d;
+	const unsigned char *s;
 
-	char_dest = (unsigned char *)dest;
-	char_src = ( const unsigned char *)src;
-	i = 0;
-
-	if(char_dest > char_src && char_dest > char_src + n) //comparação do ESPAÇO DE MEMORIAAAAAAA,
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (dest > src)
 	{
-		// usar um array auxiliar??
-		// ver se é possivel utilizar a aritmetica de endereços de memoria
-		i = n - 1;
-		while(i <= 0)
-		{
-			char_dest[i] == char_src[i];
-			i--;
-		}
+		while (n--)
+			d[n] = s[n];
 	}
-
+	else
+		ft_memcpy(dest, src, n);
+	return (dest);
 }
 
 /*
 int main(void)
 {
-	char str_a[100] = "abcdefg";
-    //char str_b[100] = "Learningisfun";
-    char *str1;
-	//char *str2 ;
-	printf("%s\n",str_a + 2);
-    str1 = str_a;
-    //str2 = str_b;
-    printf("Original string :%s\n ", str_a);
+    char *str = "abcdef";
 
-    memmove(str1 , str1, 3);
-    //t_memcpy(str2 + 8, str2, 10);
-    printf("memmove overlap : %s\n ", str_a);
-    //printf("ft_memcpy overlap : %s\n ", str_b);
+    char str_a[4] = "123";
+    char str_b[4] = "123";
+
+    printf("Original string :%s\n ", str);
+
+    memmove(str_a , str, 3);
+    ft_memmove(str_b, str, 3);
+    printf("memmove string: %s\n ", str_a);
+    printf("ft_memcpy string : %s\n ", str_b);
 }*/
 
 
