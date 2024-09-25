23cb1f2f704c444cf267679df5f3f433ee154c23
gda-conc <gabrieldaconceicaodeoliveira@gmail.com>
2024-09-25 20:40:49 -0300

    colocar a verificação em caso de input nulo

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,32 +3,33 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
-    const unsigned char	*s;
-    size_t	i;
+	const unsigned char	*s;
+	size_t	i;
 
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-    i = 0;
+	i = 0;
 
-    while (i < n)
-    {
-        d[i] = s[i];
-        i++;
-    }
-    return (dest);
+	// fazer a verificação caso dest ou src seja 0
+	// retornar nulo nesse caso
+
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
 }
 
 /*
-int main() {
-    char str_a[100] = "Learningisfun";
-    char str_b[100] = "Learningisfun";
-    char *str1, *str2 ;
-    str1 = str_a;
-    str2 = str_b;
-    printf("Original string :%s\n ", str_a);
+int main()
+{
+	char greeting1[5];
+	char greeting2[5];
 
-    memcpy(str1 + 8, str1, 10);
-    ft_memcpy(str2 + 8, str2, 10);
-    printf("memcpy overlap : %s\n ", str_a);
-    printf("ft_memcpy overlap : %s\n ", str_b);
-}*/
+	memcpy(greeting1, "Hello, world!\n", 3);
+	ft_memcpy(greeting2, "Hello, world!\n", 3);
+	printf("%s\n",greeting1);
+	printf("%s\n",greeting2);
+}
+*/
