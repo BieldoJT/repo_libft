e63269d1145bcc4cae709ff931f2398c3a59123a
BieldoJT <gabrieldaconceicaodeoliveira@gmail.com>
2024-09-26 08:40:36 -0300

    fiz as mudanças na ft_calloc, aparentemente tudo certo

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,18 +1,23 @@
-#include "libft.h"
 /* if (a * b)/b != a , then */
+#include "libft.h"
+
 
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
 	void	*ptr;
+	size_t	total_size;
 
-	if((nelem == 0) || (elsize == 0) || ((nelem * elsize)/elsize != nelem))
-		ptr = NULL;
-	else
-	{
-		ptr = (char *)malloc(nelem * elsize);
-		if(ptr != NULL)
-			ft_bzero(ptr, nelem * elsize);
-	}
+	if ((nelem == 0) || (elsize == 0))
+		return (NULL);
+	total_size = nelem * elsize;
+	if ((total_size/nelem) != elsize)
+		return (NULL);
+
+
+	ptr = (char *)malloc(total_size);
+	if(ptr != NULL)
+		ft_bzero(ptr, total_size);
+
 	return ((void *)ptr);
 }
 
